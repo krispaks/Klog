195563bb5b16a2090982e2a8003b79f18fab31a9
krispaks <krispaks@yahoo.com>
2016-03-20 00:24:53 -0500

    layouts

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/// <reference path="../node_modules/angular2/core.d.ts"/>
+
+import {Component} from '../node_modules/angular2/core';
+
+@Component({
+    selector: 'klog',
+    template: '<div></div>'
+})
+
+export class AppComponent{}
